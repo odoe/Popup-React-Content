2103f80320a7345aee4647e186ca08abe4251bee
Rene Rubalcava <rrubalcava@esri.com>
2022-03-25 13:38:39 -0700

    default portal method

--- src/PopupPortal.js
+++ src/PopupPortal.js
@@ -1,15 +1,15 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 
 const PopupPortal = ({ mount, children }) => {
-  // const el = document.createElement("div");
+  const el = document.createElement("div");
 
-  // useEffect(() => {
-  //   mount.appendChild(el);
-  //   return () => mount.removeChild(el);
-  // }, [el, mount]);
+  useEffect(() => {
+    mount.appendChild(el);
+    return () => mount.removeChild(el);
+  }, [el, mount]);
 
-  return createPortal(children, mount)
+  return createPortal(children, el);
 };
 
 export default PopupPortal;
